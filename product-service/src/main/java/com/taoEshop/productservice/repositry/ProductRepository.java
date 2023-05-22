4973c31527d64600490809e0762b2cfe51d466df
ctr34 <taoc320@gmail.com>
2023-05-22 15:10:09 +0200

    Product service has been merged into parant directory

--- product-service/src/main/java/com/taoEshop/productservice/repositry/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taoEshop.productservice.repositry;
-
-import com.taoEshop.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
